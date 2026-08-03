836c10ddf23b30c6ca5879f5f8646650634ff717
Lafnaps <177272356+Lafnaps@users.noreply.github.com>
2026-08-03 18:56:37 +0300

    Hybrid stock GPU fan: stock EC at idle, custom curve under demand

--- app/Settings.cs
+++ app/Settings.cs
@@ -22,6 +22,7 @@ namespace GHelper
 {
     public partial class SettingsForm : RForm
     {
+        readonly ToolTip sensorTip = new();
         ContextMenuStrip contextMenuStrip = new CustomContextMenu();
         ToolStripMenuItem menuEco, menuStandard, menuUltimate, menuOptimized;
         DonateControl donateControl;
@@ -196,6 +197,9 @@ namespace GHelper
 
             labelCPUFan.Click += LabelCPUFan_Click;
             labelGPUFan.Click += LabelCPUFan_Click;
+
+            sensorTip.SetToolTip(labelGPUFan, Properties.Strings.GpuRowTooltip);
+            sensorTip.SetToolTip(labelMidFan, Properties.Strings.MidRowTooltip);
 
             comboMatrix.DropDownStyle = ComboBoxStyle.DropDownList;
             comboMatrixRunning.DropDownStyle = ComboBoxStyle.DropDownList;
